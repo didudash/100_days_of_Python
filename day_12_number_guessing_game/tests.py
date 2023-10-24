72fe18491ad7a9516d74f9a993d2125aa658029b
didudash <dianaruedade@gmail.com>
2023-10-24 09:48:51 +0200

    empty tests for day 12 to complete

---
+++ day_12_number_guessing_game/tests.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest.mock import patch
+from main import guess_number, get_difficulty, LOWER_BOUND, UPPER_BOUND
+
+
+class TestNumberGuessingGame(unittest.TestCase):
+    pass
+
+
+# pending
