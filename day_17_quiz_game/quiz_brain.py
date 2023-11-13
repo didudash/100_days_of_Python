9ab5b1a10b463e718303a8f9570b8297a57f7ca0
didudash <dianaruedade@gmail.com>
2023-11-13 23:14:42 +0100

    day 17 - pending fix encoding

---
+++ day_17_quiz_game/quiz_brain.py
@@ -0,0 +1,30 @@
+# TODO: checking if the answer was correct
+# TODO: checking if we're at the end of the quiz
+
+
+class QuizBrain:
+    def __init__(self, question_list) -> None:
+        self.question_number = 0
+        self.score = 0
+        self.question_list = question_list
+
+    def still_has_questions(self) -> bool:
+        return len(self.question_list) > self.question_number
+
+    def check_answer(self, user_answer, correct_answer) -> None:
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
+
+    def next_question(self) -> None:
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(
+            f"Q. {self.question_number}: {current_question.text} (True/False): "
+        )
+        self.check_answer(user_answer, current_question.answer)
