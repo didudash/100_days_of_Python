36f1457b02b4b20fd6e63497e9cb9dd62fc1dc8a
didudash <dianaruedade@gmail.com>
2023-11-26 19:37:36 +0100

    refactored paddle

--- day_22_pong_game/main.py
+++ day_22_pong_game/main.py
@@ -9,17 +9,18 @@ screen.title("Pong Game")
 screen.tracer(0)
 
 
-right_paddle = Paddle(350)
-left_paddle = Paddle(-350)
-
-screen.listen()
-screen.onkey(right_paddle.up, "Up")
-screen.onkey(right_paddle.down, "Down")
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
 
 game_is_on = True
 while game_is_on:
     # Turning on the animation
     screen.update()
+    screen.listen()
+    screen.onkey(right_paddle.up, "Up")
+    screen.onkey(right_paddle.down, "Down")
+    screen.onkey(left_paddle.up, "x")
+    screen.onkey(left_paddle.down, "y")
 
 
 # create ball and make it move
--- day_22_pong_game/paddle.py
+++ day_22_pong_game/paddle.py
@@ -1,30 +1,19 @@
 from turtle import Turtle
 
 
-PADDLE_LENGHT = 100
-
-
-class Paddle:
-    def __init__(self, x_pos):
-        self.x_pos = x_pos
-        self.y_pos = 0
-        self.width = 20
-        self.height = 100
-        self.paddle_lenght_parts = PADDLE_LENGHT / 20
-        self.paddle_parts = []
-        self._create_paddle()
-
-    def _create_paddle(self):
-        self.paddle = Turtle("square")
-        self.paddle.color("white")
-        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.paddle.penup()
-        self.paddle.goto(self.x_pos, self.y_pos)
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
     def up(self) -> None:
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def down(self) -> None:
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
