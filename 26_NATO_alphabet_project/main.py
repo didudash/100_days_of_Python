b3ee8aa037e31ac1d767ac1dba5451401e44e609
didudash <dianaruedade@gmail.com>
2024-03-18 22:31:39 +0100

    finishing day 26

--- 26_NATO_alphabet_project/main.py
+++ 26_NATO_alphabet_project/main.py
@@ -1,1 +1,7 @@
+import pandas as pd
 
+alphabet_df = pd.read_csv("nato_phonetic_alphabet.csv")
+alphabet_dict = {row.letter: row.code for (_, row) in alphabet_df.iterrows()}
+letters = input("Enter a word: ")
+output = [alphabet_dict[letter.upper()] for letter in letters]
+print(output)
