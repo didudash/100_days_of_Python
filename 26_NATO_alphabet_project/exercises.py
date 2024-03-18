1a03efc0474502083899210b7ec5022d777f36a8
didudash <dianaruedade@gmail.com>
2024-03-18 22:31:39 +0100

    finishing day 26

--- 26_NATO_alphabet_project/exercises.py
+++ 26_NATO_alphabet_project/exercises.py
@@ -6,9 +6,7 @@ def squaring_numbers() -> None:
 
 def filtering_even_numbers() -> None:
     list_of_strings = input("Please input some integers separated by commas").split(",")
-
     list_of_ints = [int(n) for n in list_of_strings]
-
     result = [n for n in list_of_ints if n % 2 == 0]
 
     print(result)
@@ -19,11 +17,32 @@ def common_numbers(file_1: str, file_2: str) -> None:
         file_1_list = file.readlines()
     with open(file_2, "r") as file:
         file_2_list = file.readlines()
-
     result = [int(n) for n in file_1_list if n in file_2_list]
     print(result)
 
 
-# squaring_numbers()
-# filtering_even_numbers()
+def dict_comprehension() -> None:
+    sentence = input("Please input a sentence")
+    result = {word: len(word) for word in sentence.split()}
+    print(result)
+
+
+def temperature_converter(weather_c: dict) -> None:
+    weather_f = {day: (temp_c * 9 / 5) + 32 for (day, temp_c) in weather_c.items()}
+    print(weather_f)
+
+
+squaring_numbers()
+filtering_even_numbers()
 common_numbers("file1.txt", "file2.txt")
+dict_comprehension()
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+temperature_converter(weather_c)
