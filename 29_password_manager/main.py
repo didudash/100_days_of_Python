acc2e2e8fb244181b6d4073e588aa9449f1090b0
didudash <dianaruedade@gmail.com>
2024-03-23 16:20:09 +0100

    draft ui setup

--- 29_password_manager/main.py
+++ 29_password_manager/main.py
@@ -2,7 +2,17 @@ from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+def generate_password():
+    pass
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save_password():
+    pass
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -10,12 +20,41 @@ window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 # Canvas
-# Values from size in pixels of picture
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
-# Half of the width and half of the height and move x to center
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(column=1, row=0)
 
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+# website_label.config(padx=2, pady=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+# email_label.config(padx=2, pady=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+# password_label.config(padx=2, pady=2)
+
+# Entries
+website_entry = Entry(width=35)
+website_entry.insert(END, string="0")
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_entry = Entry(width=35)
+email_entry.insert(END, string="0")
+email_entry.grid(column=1, row=2, columnspan=2)
+
+password_entry = Entry(width=18)
+password_entry.insert(END, string="0")
+password_entry.grid(column=1, row=3)
+
+# Buttons
+generate_button = Button(text="Generate Password", width=18, command=generate_password)
+generate_button.grid(column=2, row=3)
+
+generate_button = Button(text="Add", width=36, command=save_password)
+generate_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
