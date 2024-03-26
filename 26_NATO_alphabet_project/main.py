7fc464576db7b9a5aeb4ccf2e482d92a83936b49
didudash <dianaruedade@gmail.com>
2024-03-26 17:35:25 +0100

    exception handling for day 26

--- 26_NATO_alphabet_project/main.py
+++ 26_NATO_alphabet_project/main.py
@@ -2,6 +2,17 @@ import pandas as pd
 
 alphabet_df = pd.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (_, row) in alphabet_df.iterrows()}
-letters = input("Enter a word: ")
-output = [alphabet_dict[letter.upper()] for letter in letters]
-print(output)
+
+
+def generate_phonetic() -> None:
+    letters = input("Enter a word: ")
+    try:
+        output = [alphabet_dict[letter.upper()] for letter in letters]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()  # Call again recursively
+    else:
+        print(output)
+
+
+generate_phonetic()
