2c6f0107d16ecc02507f5106c21e06d040158792
didudash <dianaruedade@gmail.com>
2024-04-01 21:45:52 +0200

    day 32 sending email with smtplib

--- 32_automated_bday_wisher/main.py
+++ 32_automated_bday_wisher/main.py
@@ -2,19 +2,18 @@ import smtplib
 from dotenv import load_dotenv
 import os
 
-# TODO: fix authentication error
 # Load environment variables
 load_dotenv()
 
 # Get environment variables or prompt for input if not found
 my_email = os.getenv("TEST_EMAIL") or input("Enter your email: ")
-my_password = os.getenv("TEST_PASSWORD") or input("Enter your password: ")
+my_password = os.getenv("TEST_PASSWORD") or input("Enter your app-specific password: ")
 recipient_email = os.getenv("RECIPIENT_EMAIL") or input("Enter recipient email: ")
 
 # Setup connection and send email
 try:
-    connection = smtplib.SMTP("smtp.gmail.com", 587)
-    connection.starttls()
+    # SSL (Secure Sockets Layer)
+    connection = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     connection.login(user=my_email, password=my_password)
     connection.sendmail(
         from_addr=my_email,
@@ -25,4 +24,4 @@ try:
 except Exception as e:
     print(f"An error occurred: {e}")
 finally:
-    connection.close()
+    connection.quit()
